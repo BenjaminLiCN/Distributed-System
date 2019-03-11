3b138a45414257f930f70d62d1a440147260191e
BenjaminLiCN <li.benjamincn@gmail.com>
2019-03-11 22:17:55 +1100

    git remote add origin git@github.com:BenjaminLiCN/Distributed-System.git git push -u origin master

--- Client/src/main/java/view/Client.java
+++ Client/src/main/java/view/Client.java
@@ -4,7 +4,6 @@ import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) throws Exception {
-
         String host = "127.0.0.1";
         int port = 55533;
         // 与服务端建立连接
